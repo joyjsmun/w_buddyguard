53d7317d6068c3086228bf473633ad4a41febd6b
joyjsmun <joy.js.mun@gmail.com>
2024-02-27 00:55:06 +0900

    ADD: firebase client admin setup

---
+++ pages/google.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVaHnYboccQhOTXTL3O-k8O5dQXfXyspk",
+  authDomain: "buddyguard-26449.firebaseapp.com",
+  projectId: "buddyguard-26449",
+  storageBucket: "buddyguard-26449.appspot.com",
+  messagingSenderId: "1084609737424",
+  appId: "1:1084609737424:web:c7947f3cea045ede0b64d5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
